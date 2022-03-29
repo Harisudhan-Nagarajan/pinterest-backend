1baffd19e8f6680f7b42256d7d0194955ba584d7
Harisudhan-Nagarajan <MY_NAME@example.com>
2022-03-29 20:54:26 +0530

    changes

--- getuserdetials.js
+++ getuserdetials.js
@@ -4,16 +4,15 @@ import auth from "./middleware/auth.js";
 import { userdetial, checkuser } from "./helper.js";
 const router = express.Router();
 
-router.get("/Home", auth, async (request, response) => {
+router.get("/Home", async (request, response) => {
   if (!request.header("username")) {
     response.status(400).send({ message: "failure" });
   }
   const userdetials = await userdetial(request.header("username"));
   if (userdetials.length > 0) {
-    response.status(200).send(userdetials);
+    response.status(200).send(userdetials[0]);
     return;
   }
-
   response.status(400).send({ message: "failure" });
 });
 
