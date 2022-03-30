fe9b45eaa340febae9a928043516c90e150bd05c
Harisudhan-Nagarajan <MY_NAME@example.com>
2022-03-30 18:35:20 +0530

    userdetials

--- getuserdetials.js
+++ getuserdetials.js
@@ -16,13 +16,24 @@ router.get("/Home", async (request, response) => {
   response.status(400).send({ message: "failure" });
 });
 
-router.post("/postfeed",async (request, response) => {
-    const { feed } = request.body;
-    const postfeeds = await updatefeed(request.header("username"), feed);
-    if (postfeeds.acknowledged) {
-      response.status(200).send({ message: "success" });
-      return;
-    }
-    response.status(400).send({ message: "failure" });
-  });
+router.post("/postfeed", async (request, response) => {
+  const { feed } = request.body;
+  const postfeeds = await updatefeed(request.header("username"), feed);
+  if (postfeeds.acknowledged) {
+    response.status(200).send({ message: "success" });
+    return;
+  }
+  response.status(400).send({ message: "failure" });
+});
+
+router.post("/updateuserdetial", async (request, response) => {
+  const { name, about, website, username } = request.body;
+  // const postfeeds = await updatefeed(request.header("username"), feed);
+  // if (postfeeds.acknowledged) {
+  //   response.status(200).send({ message: "success" });
+  //   return;
+  // }
+  // response.status(400).send({ message: "failure" });
+  response.send(request.body);
+});
 export const userdetials = router;
